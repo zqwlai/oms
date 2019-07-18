6499f800879a2e21da8f74d9ae8284e8d0697451
root <root@v-db-smoke05.sz.kingdee.net>
2019-07-18 15:56:54 +0800

    开启日志

--- oms/conf/default.py
+++ oms/conf/default.py
@@ -42,7 +42,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    #'common.middleware.LogRecordMiddleware',
+    'common.middleware.LogRecordMiddleware',
     'common.middleware.LoginMiddleware',
     'common.middleware.RbacMiddleware',  # 权限中间件
 )
