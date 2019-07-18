fc196e41eb476e4d4b090be39a8d77a22c1742a2
root <root@v-db-smoke05.sz.kingdee.net>
2019-07-18 15:34:24 +0800

    线上环境配置修改

--- oms/conf/settings_product.py
+++ oms/conf/settings_product.py
@@ -11,9 +11,9 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'oms_db',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': os.getenv('DB.FALCON_ENV_MYSQL_ROOT_PASSWORD', '123456'),                  # Not used with sqlite3.
-        'HOST': os.getenv('DB.FALCON_PORT_3306_TCP_ADDR', '127.0.0.1'),                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': os.getenv('DB.FALCON_PORT_3306_TCP_PORT', 3306),
+        'PASSWORD': os.getenv('DB_ENV_MYSQL_ROOT_PASSWORD', '123456'),                  # Not used with sqlite3.
+        'HOST': os.getenv('DB_PORT_3306_TCP_ADDR', '127.0.0.1'),                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': os.getenv('DB_PORT_3306_TCP_PORT', 3306),
         'OPTIONS': {
             "init_command": "SET foreign_key_checks = 0;",
         }
