41fa662a557d115024c18580be90a77c384c205f
root <root@v-db-smoke05.sz.kingdee.net>
2019-07-22 17:18:31 +0800

    线上环境配置文件优化

--- oms/conf/settings_product.py
+++ oms/conf/settings_product.py
@@ -11,7 +11,7 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'oms_db',                      # Or path to database file if using sqlite3.
         'USER': 'oms',                    # Not used with sqlite3.
-        'PASSWORD': 'Qq2345Yyt',                  # Not used with sqlite3.
+        'PASSWORD': 'Qq23#45Yyt',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': 3306,
         'OPTIONS': {
